143d8c3dd2e7b51ed05a4abd45841e621d5ed452
McNultyPT <mcnultypt@gmail.com>
2019-03-01 08:55:21 -0800

    adding post request to add a project

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,4 +5,26 @@ const knexConfig = require('../../knexfile.js');
 
 const db = knex(knexConfig.development);
 
+router.post('/', (req, res) => {
+    const projectInfo = req.body;
+
+    if(!projectInfo.name || !projectInfo.description)
+        return res.status(400).json({ errorMessage: 'Please provide a name and description for the project.' });
+
+    db('projects')
+        .insert(projectInfo)
+        .then(ids => {
+            const [id] = ids;
+
+        db('projects')
+            .where({ id })
+            .then(project => {
+                res.status(201).json(project);
+            });
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'There was an error while adding the project.' });
+        });
+});
+
 module.exports = router;
