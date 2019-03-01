1d1b408cdfe0fe98168c917a424e4ae0f2d4036a
McNultyPT <mcnultypt@gmail.com>
2019-03-01 09:08:47 -0800

    adding a post request to add an action

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
 const knex = require('knex');
 
-const knexConfig = require('../../knexfile');
+const knexConfig = require('../../knexfile.js');
 
 const db = knex(knexConfig.development);
+
+router.post('/', (req, res) => {
+    const actionInfo = req.body;
+
+    if(!actionInfo.description || ! actionInfo.project_id)
+        return res.status(400).json({ errorMessage: 'Please provide a description and project ID for the action.' });
+
+    db('actions')
+        .insert(actionInfo)
+        .then(ids => {
+            const [id] = ids;
+
+        db('actions')
+            .where({ id })
+            .then(action => {
+                res.status(201).json(action);
+            });
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'There was an error while adding the action.' });
+        });
+});
 
 module.exports = router;
